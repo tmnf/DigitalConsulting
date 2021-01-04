1e82a43b921522f27e6dd18ba9ce7e304d15f297
Tiago Farinha <tiagomnf1999@gmail.com>
2021-01-04 23:14:02 +0000

    Encryption methods created

--- DigitalPlatform/Encryption/EncryptionUtils.py
+++ DigitalPlatform/Encryption/EncryptionUtils.py
@@ -1,2 +1,11 @@
 # This class will handle all encryption matters
 # Author: Lúis Novo
+
+# Recebe array de soluções, retorna um array de soluçoes encriptadas
+def encrypt_data():
+    return None
+
+
+# Recebe um array de soluçoes encriptadas, devolve um array de soluçoes
+def decrypt_data():
+    return None
